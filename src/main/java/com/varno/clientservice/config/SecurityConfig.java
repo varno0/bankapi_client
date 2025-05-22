4a74004ddd67980b160a87a7b5b8821e15c5ab61
varno <andreika136@gmail.com>
2025-05-23 00:13:30 +0300

    Save changes

--- src/main/java/com/varno/clientservice/config/SecurityConfig.java
+++ src/main/java/com/varno/clientservice/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.varno.clientservice.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -14,28 +13,28 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                return http.httpBasic(Customizer.withDefaults())
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.
-                httpBasic(Customizer.withDefaults())
+                                .build();
+        }
 
+        @Bean
+        public UserDetailsService userDetailsService(JdbcTemplate jdbcTemplate) {
+                return username -> jdbcTemplate.query("select * from users where email=?",
+                                (rs, i) -> User.builder()
+                                                .username(rs.getString("email"))
+                                                .password(rs.getString("password"))
+                                                .authorities(
 
-                .build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService(JdbcTemplate jdbcTemplate) {
-        return username -> jdbcTemplate.query("select * from users where email=?",
-                (rs, i) -> User.builder()
-                        .username(rs.getString("email"))
-                        .password(rs.getString("password"))
-                        .authorities(
-                                jdbcTemplate.query("select * from user_authority where id=?",
-                                        (resultSet, j) ->
-                                                new SimpleGrantedAuthority(resultSet.getString("authority")),
-                                        rs.getInt("id"))
-                        )
-                        .build(), username).stream().findFirst().orElse(null);
-    }
+                                                                jdbcTemplate.query(
+                                                                                "select * from user_authority where id=?",
+                                                                                (resultSet, j) -> new SimpleGrantedAuthority(
+                                                                                                resultSet.getString(
+                                                                                                                "authority")),
+                                                                                rs.getInt("id")))
+                                                .build(),
+                                username).stream().findFirst().orElse(null);
+        }
 }
