82639bbd1864b3bb80b9a2b8d9b1b82227ede013
varno <andreika136@gmail.com>
2025-08-03 16:56:27 +0300

    xzz

--- src/main/java/com/varno/clientservice/service/ClientService.java
+++ src/main/java/com/varno/clientservice/service/ClientService.java
@@ -1,36 +1,11 @@
 package com.varno.clientservice.service;
 
-import java.util.Set;
-
-import com.varno.clientservice.exceptons.UserAlreadyExist;
-import com.varno.clientservice.repositories.RoleRepositories;
+import com.varno.clientservice.entities.Client;
 import org.springframework.stereotype.Service;
 
-import com.varno.clientservice.entities.Client;
-import com.varno.clientservice.entities.Role;
-import com.varno.clientservice.repositories.ClientRepositories;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@RequiredArgsConstructor
-@Transactional(readOnly = true)
-public class ClientService {
+public interface ClientService {
 
-    private final ClientRepositories clientRepositories;
-
-    private final RoleRepositories roleRepositories;
-
-    @Transactional
-    public Client createClient(Client client) {
-        if (clientRepositories.existsByUsername(client.getUsername())) {
-            throw new UserAlreadyExist("Username already exists");
-        }
-        Role role = roleRepositories.findByName("ROLE_USER").orElseThrow(
-                () -> new IllegalArgumentException("Role does not exist"));
-        client.setRoles(Set.of(role));
-
-        return clientRepositories.save(client);
-    }
+    Client createClient(Client client);
 }
---
+++ src/main/java/com/varno/clientservice/service/ClientServiceImpl.java
@@ -0,0 +1,36 @@
+package com.varno.clientservice.service;
+
+import java.util.Set;
+
+import com.varno.clientservice.exceptons.UserAlreadyExist;
+import com.varno.clientservice.repositories.RoleRepositories;
+import org.springframework.stereotype.Service;
+
+import com.varno.clientservice.entities.Client;
+import com.varno.clientservice.entities.Role;
+import com.varno.clientservice.repositories.ClientRepositories;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ClientServiceImpl implements ClientService {
+
+    private final ClientRepositories clientRepositories;
+
+    private final RoleRepositories roleRepositories;
+
+    @Transactional
+    public Client createClient(Client client) {
+        if (clientRepositories.existsByUsername(client.getUsername())) {
+            throw new UserAlreadyExist("Username already exists");
+        }
+        Role role = roleRepositories.findByName("ROLE_USER").orElseThrow(
+                () -> new IllegalArgumentException("Role does not exist"));
+        client.setRoles(Set.of(role));
+
+        return clientRepositories.save(client);
+    }
+}
